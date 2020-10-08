dd0a6450eede03156f892832d8dc30d04b93a977
Arduino Mandarelli <mandarelliarduino@gmail.com>
2020-10-08 16:00:27 +0100

    Initial Project Add

---
+++ src/main/java/arduino/springframework/ampetclinic/AmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package arduino.springframework.ampetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmPetClinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AmPetClinicApplication.class, args);
+  }
+
+}
