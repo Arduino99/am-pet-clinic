fc03c3dcd56a335fc69224d8846e6304f3932dec
Arduino Mandarelli <mandarelliarduino@gmail.com>
2020-10-10 20:57:36 +0100

    Added Vet List and updated controller. Closes #16.

--- pet-clinic-web/src/main/java/arduino/springframework/ampetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/arduino/springframework/ampetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package arduino.springframework.ampetclinic.controllers;
 
+import arduino.springframework.ampetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+  private final VetService vetService;
+
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
+
   @RequestMapping({"/vets", "/vets/vets", "/vets/vets.html"})
-  public String listVets(){
+  public String listVets(Model model){
+
+    model.addAttribute("vets", vetService.findAll());
     return "vets/vets";
   }
 }
